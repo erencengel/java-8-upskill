fd24613fa885585af32380b0299a74bcedcd1381
Cydeo-Saim <57418084+saimCybertek@users.noreply.github.com>
2023-10-16 13:44:53 -0500

    HR task

---
+++ src/main/java/day4_streams/Application.java
@@ -0,0 +1,12 @@
+package day4_streams;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class Application {
+    private String name;
+    private double expectedSalary;
+    private boolean willingToRelocate;
+}
---
+++ src/main/java/day4_streams/HR.java
@@ -0,0 +1,41 @@
+package day4_streams;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class HR {
+    public static void main(String[] args) {
+
+        List<Application> applications = new ArrayList<>(Arrays.asList(
+                new Application("Molly", 100_000, true),
+                new Application("James", 110_000, false),
+                new Application("Emre", 125_000, true),
+                new Application("Oleg", 120_000, false),
+                new Application("Ella", 95_000, true),
+                new Application("Poppy", 135_000, false)
+        ));
+
+        System.out.println("========== applications willing to relocate ==========");
+        applications.stream()
+                .filter(Application::isWillingToRelocate) //p -> p.isWillingToRelocate()
+                .forEach(System.out::println);
+
+        System.out.println("========== applications willing to relocate - show their names ==========");
+        applications.stream()
+                .filter(Application::isWillingToRelocate) //p -> p.isWillingToRelocate()
+                .map(Application::getName) //application -> application.getName()
+                .forEach(System.out::println);
+
+        System.out.println("========== applications are expecting less than 125,000 ==========");
+        applications.stream()
+                .filter(p -> p.getExpectedSalary() < 125_000)
+                .forEach(System.out::println);
+
+        System.out.println("========== applications are expecting less than 125,000 & willing to relocate ==========");
+        applications.stream()
+                .filter(p -> p.getExpectedSalary() < 125_000)
+                .filter(Application::isWillingToRelocate)
+                .forEach(System.out::println);
+    }
+}
